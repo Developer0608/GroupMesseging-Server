84cb666dcda8d0570bf676573d89312020b4d5f2
azadsingh99 <77088308+azadsingh99@users.noreply.github.com>
2022-02-07 18:59:08 +0530

    Final Version

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const io = require('socket.io')(server, {
 
 const db = DBConnection;
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => { 
   console.log('Call from client');
   socket.on('join-room', (user) => {
     if(!db.isUserExistInRoom(user)) {
-      console.log('User LoggedIn : ', user);
-      db.addUser(user);
+      console.log('User LoggedIn : ', user, socket.id);
+      const _user = {id: socket.id, ...user}
+      db.addUser(_user);
 
       // Push in message
       const message = {message: `${user.email} joined`, roomId: user.roomId};
@@ -23,11 +24,13 @@ io.on('connection', (socket) => {
       io.to(user.roomId).emit('new-message', message);
     } 
     let roomId = user.roomId;  
+    
     socket.join(roomId);
+
   });
 
   socket.on('get-users', (param) => {
-    const users = db.findByUserId(param.roomId);
+    const users = db.findByUserRoomId(param.roomId);
     io.to(param.roomId).emit('user-list', users);
     console.log('Users::', users);
   });
@@ -45,16 +48,33 @@ io.on('connection', (socket) => {
     io.to(roomId).emit('new-message', message);
   })
 
+  socket.on('disconnect', () => {
+    const leftUser = db.findByUserId(socket.id);
+
+    if(leftUser.length > 0) {
+      const user = leftUser[0];
+      const message = {message: `${user.email} left`, roomId: user.roomId}
+      db.addMessege(message);
+
+      db.removeUserById(user.id);
+      console.log("[DB]::disconncet::listUsers::", db.listUsers());
+
+      io.to(user.roomId).emit('user-list', db.findByUserRoomId(user.roomId));
+      io.to(user.roomId).emit('new-message', message);
+      socket.leave(user.roomId);
+    }
+  });
+
   socket.on('leave', (user) => {
     console.log('User Info : ', user);
     
-    db.RemoveUser(user);
+    db.removeUser(user);
 
     // Push in message
     const message = {message: `${user.email} left`, roomId: user.roomId}
     db.addMessege(message)
 
-    io.to(user.roomId).emit('user-list', db.findByUserId(user.roomId));
+    io.to(user.roomId).emit('user-list', db.findByUserRoomId(user.roomId));
     io.to(user.roomId).emit('new-message', message);
     socket.leave(user.roomId);
   });
